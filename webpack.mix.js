59af7c461788efd7daf60eefd3b5829a56286d2a
Ricardo Marliere <rbmarliere@gmail.com>
2017-06-18 17:38:08 -0300

    remove extract() call on webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,7 +15,4 @@ mix
     .js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .copy('resources/assets/img', 'public/img')
-    .extract([
-        'jquery'
-    ])
     .version();
