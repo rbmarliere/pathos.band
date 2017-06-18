9fb0f809575ba148aee2ff2d0a83bf2b1c452db7
Ricardo Marliere <rbmarliere@gmail.com>
2017-06-18 16:34:56 -0300

    webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix
+    .js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .copy('resources/assets/img', 'public/img')
+    .extract([
+        'jquery'
+    ])
+    .version();
