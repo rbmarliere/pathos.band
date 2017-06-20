81af3d17012285340dd786ea3a4970e1dca140d2
Ricardo Marliere <rbmarliere@gmail.com>
2017-06-19 21:31:23 -0300

    frontend

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
                     </button>
 
                     <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}" style="font-family: tattegrain; font-size: 36px; letter-spacing: 2px;">
+                    <a class="navbar-brand" href="#" v-scroll-to="'#main'" style="font-family: tattegrain; font-size: 36px; letter-spacing: 2px;">
                         {{ config('app.name') }}
                     </a>
                 </div>
@@ -55,69 +55,79 @@
             </div>
         </nav>
 
-        <div class="container-fluid">
+        <div id="main">
             <parallax :speed-factor=0.30>
                 <img src="img/pathos.jpg"/>
             </parallax>
-
         </div>
+
+        <hr/>
 
         <div id="press" class="container">
-            <div class="jumbotron">
-              <h1>Hello, world!</h1>
-              <p>...</p>
-              <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
-            </div>
         </div>
+
+        <hr/>
 
         <div id="discography" class="container">
-            <div class="jumbotron">
-              <h1>Hello, world!</h1>
-              <p>...</p>
-              <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+            <div class="row">
+                <div class="col-sm-6 text-center">
+                    <h1>Elixir</h1>
+                    <p>2015</p>
+                </div>
+                <div class="col-md-2"></div>
+                <div class="col-md-2" style="padding-left: 0px;  padding-right: 0px;">
+                    <img src="img/elixir.png" class="img-responsive">
+                </div>
             </div>
         </div>
 
+        <hr/>
+
         <div id="media" class="container">
-            media
+http://www.tribunademinas.com.br/pedrada-sonora/
+http://oganpazan.com.br/elixir-2015-pathos/
+http://doomedandstoned.com/post/126164534683/papapaulsgroovyreviews
+http://stonerking1.blogspot.com.br/2015/08/pathos-elixir-brazil-prog-stoners-debut.html
         </div>
+
+        <hr/>
 
         <div id="about" class="container">
-            about
+            <p>A Pathos é uma banda de stoner rock progressivo, de Juiz de Fora, Minas Gerais. Formada no início de 2014 por Hugo Moutinho (bateria), JP Vieira (voz), Lucas Guida (baixo) e Ricardo Marliere (guitarra). No segundo semestre de 2014 a banda passou por um árduo processo de composição, gravando seu álbum de estreia nos dias 21 e 22 de fevereiro de 2015, nomeado "Elixir". O disco foi produzido pela banda e por Arthur Damásio de forma independente e lançado em agosto de 2015, obtendo boa aceitação no Brasil e no exterior, sendo lançado, posteriormente, em vinil pelo selo alemão Krauted Mind Records. Após mais de um ano em hiato, ao fim de 2016 a banda retornou aos palcos de Juiz de Fora para participar do Festival Cultural Sangue Novo (mais de 70 artistas inscritos, sendo 20 participantes), ficando em 2° lugar. No início de 2017, Luiz Henrique Andrès (teclado) se integrou à banda.</p>
         </div>
 
+        <hr/>
+
         <div class="container">
-            <footer class="site-footer footer-light">
-                <div class="text-light">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm-6 text-center">
-                                <p class="copyright-text">
-                                    pathosound.com Copyright © 2017 by Pathos
-                                </p>
-                            </div>
-                            <div class="col-sm-2 text-right" style="top-margin: 1px;">
-                                <a target="_tab" href="https://pathos-sound.bandcamp.com/">
-                                    <icon name="bandcamp"></icon>
-                                </a>
-                                <a target="_tab" href="https://www.facebook.com/pathos.sound/">
-                                    <icon name="facebook"></icon>
-                                </a>
-                                <a target="_tab" href="https://www.instagram.com/pathos.sound/">
-                                    <icon name="instagram"></icon>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-2 text-center pagination-centered">
+                    <a target="_tab" href="https://pathos-sound.bandcamp.com/">
+                        <icon name="bandcamp" scale="4"></icon>
+                    </a>
                 </div>
-            </footer>
+                <div class="col-md-2 text-center pagination-centered">
+                    <a target="_tab" href="https://www.facebook.com/pathos.sound/">
+                        <icon name="facebook" scale="4"></icon>
+                    </a>
+                </div>
+                <div class="col-md-2 text-center pagination-centered">
+                    <a target="_tab" href="https://www.instagram.com/pathos.sound/">
+                        <icon name="instagram" scale="4"></icon>
+                    </a>
+                </div>
+                <div class="col-md-2 text-center pagination-centered">
+                    <a target="_tab" href="https://www.youtube.com/channel/UCT6Sk9JezXTip94txp0aSfQ">
+                        <icon name="youtube" scale="4"></icon>
+                    </a>
+                </div>
+                <div class="col-md-2"></div>
+            </div>
         </div>
 
     </div>
 
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}"></script>
-    <script src="{{ mix('js/manifest.js') }}"></script>
-    <script src="{{ mix('js/vendor.js') }}"></script>
 </body>
 </html>
