8392a8c6015e4975e71d2e4fd9c76d740714b95f
Ricardo Marliere <rbmarliere@gmail.com>
2017-06-30 11:19:24 -0300

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,18 +4,16 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Pathos') }}</title>
 
-    <!-- Styles -->
-    <link rel="favicon" href="{{ asset('img/favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ asset('img/elixir.jpg') }}">
+    <link rel="apple-touch-icon" href="{{ asset('img/elixir.jpg') }}">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
     <style>a{color: #343434;}</style>
 
-    <!-- Scripts -->
     <script>
         window.Laravel = {!! json_encode([
             'csrfToken' => csrf_token(),
