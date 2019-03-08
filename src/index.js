e58aeac4816980e635897eb3137a41edcd6c2946
Ricardo Marliere <rbmarliere@gmail.com>
2019-03-08 02:12:16 +0000

    add react-ga

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './views/App';
+/* eslint-disable import/first */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./views/App";
 import { BrowserRouter, Route } from "react-router-dom";
 
+// google analytics
+import ReactGA from "react-ga";
+ReactGA.initialize( "UA-135913409-1" );
+ReactGA.pageview( window.location.pathname );
+import createHistory from "history/createBrowserHistory";
+const history = createHistory();
+history.listen( (location) => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview( location.pathname );
+});
+
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter history={ history }>
         <div>
             <Route path="/" component={App} exact />
         </div>
