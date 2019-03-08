f87afccac8a61c7bc6f2479ced379dc73961189e
Ricardo Marliere <rbmarliere@gmail.com>
2019-03-08 01:59:10 +0000

    cleanup + react-router-dom

--- src/views/App.js
+++ src/views/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import logo from '../assets/logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+const App = () =>
+{
     return (
       <div className="App">
         <header className="App-header">
@@ -22,7 +22,7 @@ class App extends Component {
         </header>
       </div>
     );
-  }
-}
+};
 
 export default App;
+
