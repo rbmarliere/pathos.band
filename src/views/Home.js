d2da04725e0b3e08e9fc47399f394ed6414eb145
Ricardo Marliere <rbmarliere@gmail.com>
2019-03-14 00:46:54 +0000

    use Gallery in Home view

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,36 @@
 import React from "react";
 import App from "../components/App";
 import Biography from "../components/Biography";
+import Gallery from "../components/Gallery";
+
+// pictures
+import p1 from "../assets/image/01.jpg";
+import p2 from "../assets/image/02.jpg";
+import p3 from "../assets/image/03.jpg";
+import p4 from "../assets/image/04.jpg";
+
+const photos = [
+    {
+        src: p1,
+        width: 12,
+        height: 11
+    },
+    {
+        src: p2,
+        width: 12,
+        height: 10
+    },
+    {
+        src: p3,
+        width: 4,
+        height: 3
+    },
+    {
+        src: p4,
+        width: 14,
+        height: 9
+    }
+];
 
 const Home = () =>
 {
@@ -8,6 +38,7 @@ const Home = () =>
         <App>
             <div className="Home">
                 <Biography/>
+                <Gallery photos={ photos } />
             </div>
         </App>
     );
